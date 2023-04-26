2932a58d244a549f16ce3100af4ba4be728c76f6
jp-quintana <juanquintana1996@gmail.com>
2023-04-26 00:34:56 -0300

    WIP: Setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import notesRouter from './routes/notes.js';
 
 const app = express();
+
+app.use('/api/notes', notesRouter);
 
 app.listen(8080);
 console.log('listening on port 8080');
