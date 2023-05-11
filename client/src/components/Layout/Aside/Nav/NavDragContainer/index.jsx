7879ded7b75bbc08a9672abeae60fb233f3c37fd
jp-quintana <juanquintana1996@gmail.com>
2023-05-11 14:46:44 -0300

    WIP4: Drag

--- client/src/components/Layout/Aside/Nav/NavDragContainer/index.jsx
+++ client/src/components/Layout/Aside/Nav/NavDragContainer/index.jsx
@@ -1,10 +1,16 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 import NavElement from './NavElement';
 
 import styles from './index.module.scss';
 
 const NavDragContainer = ({ notes, selectedNote }) => {
+  const dragStartingIndex = useRef(null);
+  // const currentDragIndex = useRef(null);
+  // const [dragStartingIndex, setDragStartingIndex] = useState(null);
+  const [currentDragIndex, setCurrentDragIndex] = useState(null);
+  const [highlightIndex, setHighlightIndex] = useState(null);
+
   const handleDragStart = (e, index) => {
     const targetRect = e.currentTarget.getBoundingClientRect();
     const xOffset = e.clientX - targetRect.left;
@@ -12,8 +18,16 @@ const NavDragContainer = ({ notes, selectedNote }) => {
 
     e.dataTransfer.setDragImage(e.currentTarget, xOffset, yOffset);
 
-    console.log(index);
+    setCurrentDragIndex(index);
+    dragStartingIndex.current = index;
   };
+  const handleDragStop = (e) => {
+    dragStartingIndex.current = null;
+    setCurrentDragIndex(null);
+    setHighlightIndex(null);
+  };
+
+  console.log(dragStartingIndex, currentDragIndex, highlightIndex);
 
   return (
     <div className={styles.container}>
@@ -21,23 +35,32 @@ const NavDragContainer = ({ notes, selectedNote }) => {
         <li
           draggable
           onDragStart={(e) => handleDragStart(e, index)}
+          onDragEnd={handleDragStop}
           className={
             selectedNote && selectedNote.id === note.id
               ? styles.isSelected
               : undefined
           }
+          // style={{
+          //   position:
+          //     index === currentDragIndex &&
+          //     currentDragIndex !== dragStartingIndex &&
+          //     'relative',
+          // }}
+          style={{
+            position: 'relative',
+          }}
           key={note.id}
         >
           <div
-            onDragEnter={(e) => {
-              console.log('enter', index);
+            onDragEnter={() => {
+              setCurrentDragIndex(index);
             }}
-            onDragLeave={(e) => {
-              console.log('leaving', index);
+            onDragLeave={() => {
+              setCurrentDragIndex(index);
             }}
             onDragOver={(e) => {
               e.preventDefault();
-              console.log('hey');
             }}
           >
             <NavElement
@@ -49,7 +72,31 @@ const NavDragContainer = ({ notes, selectedNote }) => {
               ellipsisClassName={styles.ellipsis}
             />
           </div>
-          <div></div>
+          {index === currentDragIndex &&
+            currentDragIndex !== dragStartingIndex.current && (
+              <div
+                className={styles.highlight_wrapper}
+                onDragEnter={() => {
+                  setHighlightIndex(index);
+                }}
+                onDragLeave={() => {
+                  setHighlightIndex(null);
+                }}
+                onDragOver={(e) => {
+                  e.preventDefault();
+                }}
+                style={{
+                  top: index < dragStartingIndex.current ? '-1.4rem' : 'auto',
+                  bottom:
+                    index > dragStartingIndex.current ? '-1.4rem' : 'auto',
+                  opacity: highlightIndex === index ? 1 : 0,
+                }}
+              >
+                <div className={styles.highlight} />
+              </div>
+            )}
+
+          {/* <div className={styles.highlight} style={{ top: '-.4rem' }} /> */}
         </li>
       ))}
     </div>
